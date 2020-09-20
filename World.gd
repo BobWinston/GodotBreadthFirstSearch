30dcae956fb5fe89a068445f805f4d8f8d0917ca
BobWinston <68573633+BobWinston@users.noreply.github.com>
2020-09-19 22:41:54 -0700

    Radius Pathfinding
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- World.gd
+++ World.gd
@@ -2,14 +2,20 @@ extends Node2D
 
 onready var tilemap = $TileMap
 onready var player = $Player
+onready var step_count_limit = 3
+
+
 
 func _ready():
 	pass # Replace with function body.
+	
+	
+	
 
 func _process(delta):
 	if Input.is_action_just_pressed("click"):
 		mouse_pos = global_position_to_tilemap_pos(get_global_mouse_position())
-		var player_pos = global_position_to_tilemap_pos(player.global_position)
+		player_pos = global_position_to_tilemap_pos(player.global_position)
 		path = []
 		path = yield(get_path_bfs(player_pos, mouse_pos), "completed")
 		#path = get_path_bfs(player_pos, mouse_pos)
@@ -21,19 +27,23 @@ var path = []
 var mouse_pos = Vector2()
 var path_display = []
 var reached_end = false
+var player_pos = Vector2()
 func _draw():
 	for cell_info in visited_data_for_display:
 		var cell_pos = tilemap.map_to_world(Vector2(cell_info.pos.x, cell_info.pos.y)) + Vector2(8, 8)
 		if reached_end:
-			draw_circle(cell_pos, 5, Color.lightsalmon)
+			#draw_rect(Rect2(cell_pos - Vector2(8,8),Vector2(15,15)), Color.lightsalmon)
+			draw_circle(cell_pos,5,Color.lightsalmon)
 		else:
+			draw_rect(Rect2(cell_pos - Vector2(8,8),Vector2(15,15)), Color.lightsalmon)
 			draw_circle(cell_pos, 5, Color.red)
+			
 		if cell_info.last_pos != null:
 			var last_cell_pos = tilemap.map_to_world(Vector2(cell_info.last_pos.x, cell_info.last_pos.y)) + Vector2(8, 8)
-			if reached_end:
-				draw_line(cell_pos, last_cell_pos, Color.lightgreen, 2)
-			else:
-				draw_line(cell_pos, last_cell_pos, Color.green, 2)
+			#if reached_end:
+				#draw_line(cell_pos, last_cell_pos, Color.lightgreen, 2)
+			#else:
+				#draw_line(cell_pos, last_cell_pos, Color.green, 2)
 	draw_circle(tilemap.map_to_world(Vector2(mouse_pos.x, mouse_pos.y)) + Vector2(8, 8), 5, Color.yellow)
 	var last_cell_pos = null
 	if len(path_display) > 0:
@@ -51,8 +61,12 @@ func global_position_to_tilemap_pos(pos):
 	var t_pos = tilemap.world_to_map(pos)
 	return {"x": int(round(t_pos.x)), "y": int(round(t_pos.y))}
 
+
 func can_move_to_spot(cell_pos):
-	return tilemap.get_cell(cell_pos.x, cell_pos.y) < 0
+	var step_count = abs(cell_pos.x - player_pos.x) + abs(cell_pos.y - player_pos.y)
+	#gay += 1
+	#print(gay)
+	return (tilemap.get_cell(cell_pos.x, cell_pos.y) < 0) and (step_count <= step_count_limit)
 
 const DISPLAY_RATE = 0.01
 const PATH_DISPLAY_RATE = 0.1
@@ -75,15 +89,15 @@ func get_path_bfs(start_pos, goal_pos):
 		var cell_info = queue.pop_front()
 		if check_cell(cell_info.pos, cell_info.last_pos, goal_pos):
 			reached_end = true
-			break
+			#break
 		iters += 1
 		if iters >= MAX_ITERS:
 			return []
 		yield(get_tree().create_timer(DISPLAY_RATE), "timeout")
-		update()
+		update() #tab for real time processing visualization
 	var backtraced_path = []
 	var cur_pos = goal_pos
-	while str(cur_pos) in visited and visited[str(cur_pos)] != null:
+	while false:#str(cur_pos) in visited and visited[str(cur_pos)] != null: #draw blue line
 		yield(get_tree().create_timer(PATH_DISPLAY_RATE), "timeout")
 		update()
 		if cur_pos != null:
@@ -102,8 +116,8 @@ func check_cell(cur_pos, last_pos, goal_pos):
 	
 	visited[str(cur_pos)] = last_pos
 	visited_data_for_display.append({"pos": cur_pos, "last_pos": last_pos})
-	if cur_pos.x == goal_pos.x and cur_pos.y == goal_pos.y:
-		return true
+	#if cur_pos.x == goal_pos.x and cur_pos.y == goal_pos.y: 
+		#return true
 	
 	queue.push_back({"pos": {"x": cur_pos.x, "y": cur_pos.y + 1}, "last_pos": cur_pos})
 	queue.push_back({"pos": {"x": cur_pos.x + 1, "y": cur_pos.y}, "last_pos": cur_pos})
